af77445091d9abcb55f18dcf027946ac4451a33a
WindowsShadows <68105821+WindowsShadows@users.noreply.github.com>
2022-05-20 21:11:05 +0200

    Portal 2 code

--- src/game/shared/multiplayer/basenetworkedplayer_shared.cpp
+++ src/game/shared/multiplayer/basenetworkedplayer_shared.cpp
@@ -5,6 +5,10 @@
 #include "multiplayer/basenetworkedplayer_cl.h"
 #endif
 
+ConVar player_normspeed("player_normspeed", "175", FCVAR_NEVER_AS_STRING | FCVAR_REPLICATED);
+ConVar player_walkspeed("player_walkspeed", "175", FCVAR_NEVER_AS_STRING | FCVAR_REPLICATED);
+ConVar player_crouchspeed("player_crouchspeed", "58.33", FCVAR_NEVER_AS_STRING | FCVAR_REPLICATED);
+
 void CBaseNetworkedPlayer::MakeAnimState()
 {
 #ifdef CLIENT_DLL
@@ -12,8 +16,8 @@ void CBaseNetworkedPlayer::MakeAnimState()
 #endif
 	MultiPlayerMovementData_t mv;
 	mv.m_flBodyYawRate = 360;
-	mv.m_flRunSpeed = 320;
-	mv.m_flWalkSpeed = 75;
+	mv.m_flRunSpeed = player_normspeed.GetInt();
+	mv.m_flWalkSpeed = player_walkspeed.GetInt();
 	mv.m_flSprintSpeed = -1.0f;
 	m_PlayerAnimState = new CMultiPlayerAnimState( this,mv );
 }
